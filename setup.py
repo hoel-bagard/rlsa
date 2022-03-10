7d1e505274264a93f7ae22617ca9088ce0bb04eb
Hoel Bagard <hoel.bagard@gmail.com>
2022-03-10 15:59:09 +0900

    Working on the publishing part.

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-
-    config = Configuration()
-    config.add_extension("rlsa", ["rlsa/rlsa.c"])
-
-    return config
+import numpy as np
+import setuptools
 
 
-if __name__ == "__main__":
-    from numpy.distutils.core import setup
-    setup(configuration=configuration)
+rlsa_module = setuptools.Extension("rlsa", sources=["rlsa/rlsa.c"], include_dirs=[np.get_include()])
+
+
+setuptools.setup(
+    name="rlsa",
+    version="0.0.1",
+    description="Run Length Smoothing Algorithm",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/hoel-bagard/rlsa",
+    author="Hoel Bagard",
+    classifiers=["Programming Language :: Python :: 3"],
+    keywords="rlsa",
+    zip_safe=False,
+    install_requires=["numpy"],
+    ext_modules=[rlsa_module],
+    packages=["rlsa"],
+    include_package_data=True,
+)
+
+
+# def configuration(parent_package='', top_path=None):
+#     from numpy.distutils.misc_util import Configuration
+
+#     config = Configuration()
+#     config.add_extension("rlsa", ["rlsa/rlsa.c"])
+
+#     return config
+
+
+# if __name__ == "__main__":
+#     from numpy.distutils.core import setup
+#     setup(configuration=configuration)
