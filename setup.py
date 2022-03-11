c45c58cc9bfb171f8f2f1e5549a77f29df560e8e
Hoel Bagard <hoel.bagard@gmail.com>
2022-03-11 13:51:10 +0900

    Version bump.

--- setup.py
+++ setup.py
@@ -6,13 +6,19 @@ rlsa_module = setuptools.Extension("rlsa", sources=["rlsa/rlsa.c"], include_dirs
 
 setuptools.setup(
     name="rlsa",
-    version="0.0.1",
+    version="0.0.2",
     description="Run Length Smoothing Algorithm",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type='text/markdown',
     url="https://github.com/hoel-bagard/rlsa",
     author="Hoel Bagard",
-    classifiers=["Programming Language :: Python :: 3"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+    ],
     keywords="rlsa",
     zip_safe=False,
     install_requires=["numpy"],
